84dfa423d9b95b38aa517f1c88dd081b6c824ff1
eiken0525 <eiken0525@gmail.com>
2024-06-12 17:04:33 +0800

    2024/6/12 17:05

---
+++ py/example_eq_for_LA.py
@@ -0,0 +1,9 @@
+import plot_tool
+
+def L1(x, y):
+    return 2 * x - y
+
+def L2(x, y):
+    return -x + 2 * y
+
+plot_tool.plot_implicit_functions([L1, L2], "Row Picture", [0, 3], ["$2x-y=0$", "$-x+2y=3$"], (-3, 3), (0, 6), "example equation for the essense of linear algebra", 18)
---
+++ py/plot_tool.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_implicit_functions(functions, title, contours, labels, x_interval, y_interval, graph_name, font_size, delicacy=int(1e3)):
+    colors = ["blue", "olive", "violet", "navy", "lime", "slategrey", "hotpink"][:len(contours)]
+    # See https://matplotlib.org/stable/gallery/color/named_colors.html
+
+    def make_grid(alpha, beta):
+        try:
+            return np.meshgrid(alpha, beta)
+        except np.core._exceptions._ArrayMemoryError:
+            alpha_midpoint = (alpha[0] + alpha[1]) / 2
+            beta_midpoint = (beta[0] + beta[1]) / 2
+            grid_a = make_grid((alpha[0], alpha_midpoint), (beta[0], beta_midpoint))
+            grid_b = make_grid((alpha_midpoint, alpha[1]), (beta[0], beta_midpoint))
+            grid_c = make_grid((alpha_midpoint, alpha[1]), (beta_midpoint, beta[1]))
+            grid_d = make_grid((alpha[0], alpha_midpoint), (beta_midpoint, beta[1]))
+            return grid_a[0]+grid_b[0]+grid_c[0]+grid_d[0], grid_a[1]+grid_b[1]+grid_c[1]+grid_d[1]
+        
+    ratio = (y_interval[1] - y_interval[0]) / (x_interval[1] - x_interval[0])
+    if ratio > 1:
+        plt.figure(figsize=(10, 10 * ratio))
+    else:
+        plt.figure(figsize=(10 / ratio, 10))
+    x = np.linspace(x_interval[0], x_interval[1], delicacy)
+    y = np.linspace(y_interval[0], y_interval[1], delicacy)
+    X, Y = make_grid(x, y)
+    for function, contour, color, label in zip(functions, contours, colors, labels):
+        Z = function(X, Y)
+        plt.contour(X, Y, Z, levels=[contour], colors=color)
+        plt.plot([], [], color=color, label=label)
+    plt.xlabel('$x$', fontsize=font_size)
+    plt.ylabel('$y$', fontsize=font_size)
+    plt.xticks(fontsize=font_size)
+    plt.yticks(fontsize=font_size)
+    plt.title(title, fontsize=font_size)
+    plt.grid(True)
+    plt.legend(fontsize=font_size)
+    plt.savefig(f"{graph_name}.png", transparent=True)
